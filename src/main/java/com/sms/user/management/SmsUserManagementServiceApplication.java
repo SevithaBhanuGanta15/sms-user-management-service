62405469396e6c06341feb8265c289af7abb5f14
SevithaBhanuGanta15 <gsevithabhanu15@gmail.com>
2024-07-21 11:56:12 +0530

    Initial Commit

---
+++ src/main/java/com/sms/user/management/SmsUserManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sms.user.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsUserManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsUserManagementServiceApplication.class, args);
+	}
+
+}
